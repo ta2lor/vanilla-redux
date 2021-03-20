4fbe0bec8dbf414a72d2edddd9d2e1c7565f5112
taylor2613! <ksh262011@naver.com>
2021-03-20 20:14:34 +0900

    0320

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const plus = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.getElementById("number");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModify = (state = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return state + 1;
-    case MINUS:
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-const countStore = createStore(countModify);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-};
-
-countStore.subscribe(onChange);
-
-plus.addEventListener("click", () => countStore.dispatch({ type: ADD }));
-minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
-
-console.log(countStore);
+ReactDOM.render(<App></App>, document.getElementById("root"));
