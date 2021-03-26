a4571d87b5e0bdc78f3231e7c200249938639a38
taylor2613! <ksh262011@naver.com>
2021-03-26 12:37:35 +0900

    0326

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
-ReactDOM.render(<App></App>, document.getElementById("root"));
+import { Provider } from "react-redux";
+import store from "../src/Routes/store";
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
